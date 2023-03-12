467390c986ea39e3a28502956e8304cd3784feee
Ashwin Preyan M <ashwinpreyan@gmail.com>
2023-03-12 23:53:46 +0530

    fv

--- src/Buy/Buy.js
+++ src/Buy/Buy.js
@@ -1,81 +1,28 @@
 import React from 'react'
 import './Buy.scss'
+import Card from './Card';
+
+import data from './data.json';
+
+const tests = data.data;
+
+
 
 function Buy() {
   return (
     <div className="hel">
-    <div class="container">
-            <div class="outer">
-                <div id="card1" class="card">
-                    <div class="bg1 imgbg"></div>
-                    <div class="line">
-                        <h2>Lost in the Beauty of the Ocean</h2>
-                        <p>There's nothing quite like the feeling of being by the ocean. The sound of the waves crashing against the shore, the salty smell of the sea air, and the vast expanse of the ocean itself can all make us feel small yet connected to something much greater than ourselves.</p>
-                        <button id="btn1" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
-            <div class="outer">
-                <div id="card2" class="sec card">
-                    <div class="bg2 imgbg"></div>
-                    <div class="line">
-                        <h2>Find Your Inner Peace in the Mountains</h2>
-                        <p>There's something about the mountains that just feels like home. The crisp mountain air, the towering peaks, and the peaceful stillness of nature can all make us feel more grounded and connected to the world around us.</p>
-                        <button id="btn2" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
-        
-        <div class="outer">
-            <div id="card3" class="card">
-                <div class="bg3 imgbg"></div>
-                    <div class="line">
-                        <h2>Embrace the Serenity of the Desert</h2>
-                        <p>The desert may seem barren and lifeless at first glance, but it's actually teeming with a unique kind of beauty and life. The vast stretches of sand and rock formations can create stunning landscapes that are unlike anything you've ever seen before.</p>
-                        <button id="btn3" class="mr">Learn More</button>
-                    </div>
-            </div>
+        <div className='filter'>
+
         </div>
-        <div class="outer">
-                <div id="card1" class="card">
-                    <div class="bg1 imgbg"></div>
-                    <div class="line">
-                        <h2>Lost in the Beauty of the Ocean</h2>
-                        <p>There's nothing quite like the feeling of being by the ocean. The sound of the waves crashing against the shore, the salty smell of the sea air, and the vast expanse of the ocean itself can all make us feel small yet connected to something much greater than ourselves.</p>
-                        <button id="btn1" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
-            <div class="outer">
-                <div id="card2" class="sec card">
-                    <div class="bg2 imgbg"></div>
-                    <div class="line">
-                        <h2>Find Your Inner Peace in the Mountains</h2>
-                        <p>There's something about the mountains that just feels like home. The crisp mountain air, the towering peaks, and the peaceful stillness of nature can all make us feel more grounded and connected to the world around us.</p>
-                        <button id="btn2" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
-            <div class="outer">
-                <div id="card1" class="card">
-                    <div class="bg1 imgbg"></div>
-                    <div class="line">
-                        <h2>Lost in the Beauty of the Ocean</h2>
-                        <p>There's nothing quite like the feeling of being by the ocean. The sound of the waves crashing against the shore, the salty smell of the sea air, and the vast expanse of the ocean itself can all make us feel small yet connected to something much greater than ourselves.</p>
-                        <button id="btn1" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
-            <div class="outer">
-                <div id="card2" class="sec card">
-                    <div class="bg2 imgbg"></div>
-                    <div class="line">
-                        <h2>Find Your Inner Peace in the Mountains</h2>
-                        <p>There's something about the mountains that just feels like home. The crisp mountain air, the towering peaks, and the peaceful stillness of nature can all make us feel more grounded and connected to the world around us.</p>
-                        <button id="btn2" class="mr">Learn More</button>
-                    </div>
-                </div>
-            </div>
+    <div class="container">    
+        {tests.map((item) => (
+          <Card
+            name={item.name}
+           head={item.head}
+            cont={item.cont}
+            bg = {item.back}
+          />
+        ))}
     </div>
     </div>
   )
--- src/Buy/Card.js
+++ src/Buy/Card.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import data from'data.json';
+import { Link } from "react-router-dom";
 
 
-function Card() {
-  return (
-    <div></div>
-  )
-}
+const Card = ({name, head, cont, bg}) => (
+  <div class="outer">
+              <div id="card1" class="card">
+                  <div class={bg+ " imgbg"}></div>
+                  <div class="line">
+                      <h2>{head}</h2>
+                      <p>{cont}</p>
+                      <Link to={"/buy/"+name}><button id="btn1" class="mr">Learn More</button></Link>
+                  </div>
+              </div>
+          </div>
+);
 
 export default Card
